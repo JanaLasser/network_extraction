58400d6821ae50bd7b86763ba52aa287891f5312
JanaLasser <jana.lasser@ds.mpg.de>
2015-06-17 17:03:34 +0200

    finished the analyze script

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -19,12 +19,12 @@
     jana.lasser@ds.mpg.de
 '''
 import argparse
-
 import networkx as nx
 import numpy as np
-import scipy
+import scipy.spatial
+import ntpath
+from os.path import join
 
-'''
 parser = argparse.ArgumentParser(description='Analyze: a program that ' +\
     'calculates basic statistics of a graph.\nCopyright (C) 2015 Jana Lasser')
 
@@ -36,11 +36,16 @@ parser.add_argument('-dest', type=str, help='Complete path to the folder '\
 args = parser.parse_args()
 source = args.source
 dest = args.dest
-'''
+
+graph_name = image_name = ntpath.basename(source).split('.')[0]
+if dest == None:
+    dest = source.split(graph_name)[0]
+graph_name = graph_name.split('_graph')[0]
+
+G = nx.read_gpickle(source)
 
 def NumberOfJunctions(G):
-    junctions = 0
-    
+    junctions = 0    
     for n in G.nodes():
         if type(G) == type(nx.DiGraph):
             if len(G.neighbors(n)) >= 2:
@@ -52,8 +57,7 @@ def NumberOfJunctions(G):
     return junctions
     
 def NumberOfTips(G):
-    tips = 0
-    
+    tips = 0  
     for n in G.nodes():
         if type(G) == type(nx.DiGraph):
             if len(G.neighbors(n)) == 0:
@@ -65,26 +69,41 @@ def NumberOfTips(G):
     return tips
     
 def TotalLength(G):
-    return np.asarray([e[2]['length'] for e in G.edges_iter(data=True)]).sum()
+    return np.asarray([e[2]['weight'] for e in G.edges_iter(data=True)]).sum()
     
 def AverageEdgeLength(G):
-    return np.asarray([e[2]['length'] for e in G.edges_iter(data=True)]).mean()
+    return np.asarray([e[2]['weight'] for e in G.edges_iter(data=True)]).mean()
     
 def AverageEdgeRadius(G):
-    return np.asarray([e[2]['radius'] for e in G.edges_iter(data=True)]).mean()
+    return np.asarray([e[2]['conductivity'] for e in G.edges_iter(data=True)]).mean()
     
 def TotalNetworkArea(G):
-    return np.asarray([e[2]['length']*e[2]['radius'] \
+    return np.asarray([e[2]['weight']*e[2]['conductivity'] \
         for e in G.edges_iter(data=True)]).sum()
     
 def AreaOfConvexHull(G):
-    points = [[n[1]['y'],n[1]['x']] for n in G.nodes(data=True)]    
+    points = np.asarray([[n[1]['y'],n[1]['x']] for n in G.nodes(data=True)])   
     hull = scipy.spatial.ConvexHull(points)
     vertices = points[hull.vertices]
     vertices = np.vstack([vertices,vertices[0,0:]])
     lines = np.hstack([vertices,np.roll(vertices,-1,axis=0)])
     area = 0.5*abs(sum(x1*y2-x2*y1 for x1,y1,x2,y2 in lines))
     return area
+
+def NumberOfCycles(G):
+    return len(nx.cycle_basis(G))
+
+f = open(join(dest,'network_statistics_' + graph_name + '.txt'), 'w')
+f.write('*** Statistics for ' + graph_name + ' ***\n\n')
+f.write('Number of junctions:\t %d\n'%NumberOfJunctions(G))
+f.write('Number of tips:\t\t %d\n'%NumberOfTips(G))
+f.write('Total length:\t\t %f px\n'%TotalLength(G))
+f.write('Average edge length:\t %f px\n'%AverageEdgeLength(G))
+f.write('Average edge radius:\t %f px\n'%AverageEdgeRadius(G))
+f.write('Total network area:\t %f px^2\n'%TotalNetworkArea(G))
+f.write('Area of convex hull:\t %f px^2\n'%AreaOfConvexHull(G))
+f.write('Number of cycles:\t %d\n'%NumberOfCycles(G)) 
+f.close()
 
 
 
